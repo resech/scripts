4e704f560e64461979a43ac673d30f718dfdf47f
Ross Sechler <resech@chesssorrel.us>
2018-05-23 10:14:31 +1000

    Added copy step

--- zap_weekly_update.sh
+++ zap_weekly_update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-INSTALLED_DIR="/Users/rsechler/ZAProxy"
+INSTALLED_DIR="/tmp/ZAP_PROXY"
 
 CURRENT=`curl -s "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" | grep daily-version | awk -v FS="(<daily-version>|</daily-version>)" '{print $2}'`
+INSTALLED=`cat $INSTALLED_DIR/installed.txt`
 
 if [ ! -f $INSTALLED_DIR/installed.txt ]; then
     INSTALLED='0'
@@ -14,9 +15,12 @@ if [ $INSTALLED != $CURRENT ]; then
     TEMPFILE=`mktemp`
     URL=`curl -s "https://raw.githubusercontent.com/zaproxy/zap-admin/master/ZapVersions.xml" | grep "ZAP_WEEKLY_D" | grep "<url>" | awk -v FS="(<url>|</url>)" '{print $2}'`
     wget $URL -O $TEMPFILE; unzip -d $INSTALLED_DIR $TEMPFILE; rm $TEMPFILE
-    mv $INSTALLED_DIR/ZAP_$CURRENT $INSTALLED_DIR/ZAP_Files
+    cp -Rf $INSTALLED_DIR/ZAP_$CURRENT/* $INSTALLED_DIR/ZAP_Files
+    rm -rf $INSTALLED_DIR/ZAP_$CURRENT
+    rm -rf $INSTALLED_DIR/ZAP_Files/zap-$INSTALLED.jar
     INSTALLED=$CURRENT
-    echo $CURRENT > $INSTALLED_DIR/installed.txt
+    echo $INSTALLED > $INSTALLED_DIR/installed.txt
+    echo "Upgrade complete, Installed version is $INSTALLED."
 else
     echo "Already up to date"
 fi
